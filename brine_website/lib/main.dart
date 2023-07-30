f06638890aa4b4228729008375c637bea8ee98e5
Scott Hatfield <contact@hatfieldscott.com>
2023-07-30 11:06:27 -0500

    Removed unused dependency :seal:

--- brine_website/lib/main.dart
+++ brine_website/lib/main.dart
@@ -32,7 +32,9 @@ Future<void> main() async {
   }
 
   // Pass all uncaught errors from the framework to Crashlytics.
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  if (!kIsWeb) {
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  }
 
   runApp(const BrineMonitorWebsite());
 }
