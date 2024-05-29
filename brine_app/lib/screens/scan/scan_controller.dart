b560eb59e5736b1d9d0f7bbb8f34151764f85fad
Scott Hatfield <contact@hatfieldscott.com>
2024-05-28 22:05:32 -0500

    Check for Bluetooth permissions and status :smile_cat:

--- brine_app/lib/screens/scan/scan_controller.dart
+++ brine_app/lib/screens/scan/scan_controller.dart
@@ -4,7 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_splendid_ble/central/models/scan_filter.dart';
 import 'package:flutter_splendid_ble/central/splendid_ble_central.dart';
 import 'package:flutter_splendid_ble/shared/models/ble_device.dart';
+import 'package:flutter_splendid_ble/shared/models/bluetooth_permission_status.dart';
+import 'package:flutter_splendid_ble/shared/models/bluetooth_status.dart';
 
+import '../../services/analytics/analytics.dart';
+import '../setup/setup_route.dart';
 import 'scan_route.dart';
 import 'scan_view.dart';
 
@@ -13,17 +17,87 @@ class ScanController extends State<ScanRoute> {
   /// An instance of the [SplendidBle] service used for the Bluetooth scanning process.
   final SplendidBle _ble = SplendidBle();
 
+  /// A [StreamSubscription] used to listen for changes in the state of the Bluetooth adapter.
+  ///
+  /// This subscription listens for updates on the current status of the Bluetooth adapter,
+  /// such as whether it is turned on or off.
+  StreamSubscription<BluetoothStatus>? _bluetoothStatusStream;
+
+  /// The current status of the Bluetooth adapter, represented by the [BluetoothStatus] enum.
+  BluetoothStatus? _bluetoothAdapterStatus;
+
+  /// A [StreamSubscription] used to listen for changes in the state of the Bluetooth permissions on the host platform.
+  StreamSubscription<BluetoothPermissionStatus>? _bluetoothPermissionStream;
+
+  /// The current status of the Bluetooth permissions on the host platform, represented by the
+  /// [BluetoothPermissionStatus] enum.
+  BluetoothPermissionStatus? _bluetoothPermissionStatus;
+
   /// A [StreamSubscription] allowing the controller to listen for newly discovered BLE devices.
-  late StreamSubscription<BleDevice> _discoveredDeviceSubscription;
+  StreamSubscription<BleDevice>? _discoveredDeviceSubscription;
 
   /// A [Timer] used to implement a timeout for the scanning process.
   late Timer _scanTimeout;
 
   @override
   void initState() {
-    _startScan();
+    // Initialize the Bluetooth permission status monitor.
+    _initBluetoothPermissionStatusMonitor();
 
     super.initState();
+  }
+
+  /// Initializes Bluetooth permission status monitoring.
+  ///
+  /// This method sets up a listener to monitor the current status of the Bluetooth permissions on the host platform.
+  void _initBluetoothPermissionStatusMonitor() {
+    _bluetoothPermissionStream = _ble.emitCurrentPermissionStatus().listen(
+      (status) {
+        _bluetoothPermissionStatus = status;
+
+        // If permissions are granted, start monitoring the Bluetooth adapter status.
+        if (_bluetoothPermissionStatus == BluetoothPermissionStatus.granted) {
+          _initBluetoothAdapterStatusMonitor();
+        }
+      },
+      onError: (error) {
+        // TODO(Toglefritz): go to a Bluetooth error screen
+      },
+    );
+
+    // Request Bluetooth permissions. If they have already been granted, this method will do nothing.
+    _ble.requestBluetoothPermissions();
+  }
+
+  /// Initializes Bluetooth status monitoring.
+  ///
+  /// This method sets up a listener to monitor the current status of the Bluetooth adapter. It is typically called
+  /// during the initialization phase of the app or when Bluetooth monitoring is required.
+  void _initBluetoothAdapterStatusMonitor() {
+    try {
+      _bluetoothStatusStream = _ble.emitCurrentBluetoothStatus().listen(
+        (status) {
+          _bluetoothAdapterStatus = status;
+
+          // Start the scan if the Bluetooth adapter is available and if permissions have been granted.
+          if (_bluetoothAdapterStatus == BluetoothStatus.enabled &&
+              _bluetoothPermissionStatus == BluetoothPermissionStatus.granted) {
+            _startScan();
+          }
+        },
+        onError: (error) {
+          // TODO(Toglefritz): go to a Bluetooth error screen
+        },
+      );
+    } catch (e) {
+      debugPrint('Unable to get Bluetooth status with exception, $e');
+
+      setState(() {
+        _bluetoothAdapterStatus = BluetoothStatus.notAvailable;
+      });
+
+      // TODO(Toglefritz): go to a Bluetooth error screen
+    }
   }
 
   /// Starts the scan for nearby BLE devices.
@@ -42,7 +116,7 @@ class ScanController extends State<ScanRoute> {
 
     // Start the scan
     _discoveredDeviceSubscription = _ble.startScan(
-    filters: <ScanFilter>[
+      filters: <ScanFilter>[
         ScanFilter(
           serviceUuids: ['6272696e-6573-616c-746d-6f6e69746f72'],
         ),
@@ -57,25 +131,55 @@ class ScanController extends State<ScanRoute> {
   /// Receives [BleDevice] instances from the [SplendidBle] service that represent BLE devices discovered during
   /// the scan. Since the scan is filtered to only show devices with the Brine service UUID, this method will only be
   /// called when Brine devices are discovered.
+  // TODO(Toglefritz): stop scanning once Brine device is found
   void _onDeviceDiscovered(BleDevice device) {
     debugPrint('Discovered device: ${device.name}');
 
     // Cancel the timeout timer.
-    _scanTimeout.cancel();
+    //_scanTimeout.cancel();
 
     // Stop the scan.
-    _stopScan();
+    //_stopScan();
 
     // Navigate to the next screen.
     // TODO(Toglefritz): implement
   }
 
+  /// Handles taps on the "cancel" button used to stop the scan and return to the setup route so the account can be
+  /// loaded again.
+  void onCancelScan() {
+    Analytics.trackEvent(eventName: 'scan_cancel_tap');
+
+    _stopScan();
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) => const SetupRoute(),
+      ),
+    );
+  }
+
   /// Stops the scan for nearby BLE devices.
   void _stopScan() {
     _ble.stopScan();
-    _discoveredDeviceSubscription.cancel();
+    _discoveredDeviceSubscription?.cancel();
   }
 
   @override
   Widget build(BuildContext context) => ScanView(this);
+
+  @override
+  void dispose() {
+    // Stop the scan.
+    _stopScan();
+
+    // Cancel the Bluetooth status stream.
+    _bluetoothStatusStream?.cancel();
+
+    // Cancel the Bluetooth permission stream.
+    _bluetoothPermissionStream?.cancel();
+
+    super.dispose();
+  }
 }
--- brine_app/lib/screens/scan/scan_view.dart
+++ brine_app/lib/screens/scan/scan_view.dart
@@ -15,6 +15,17 @@ class ScanView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.cancel_outlined,
+              color: Theme.of(context).primaryColorDark,
+            ),
+            onPressed: state.onCancelScan,
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
