c2443a851925ebb5f5f87752cb23acfd441368de
Scott Hatfield <contact@hatfieldscott.com>
2024-08-27 16:49:51 -0500

    Accept Brine public key in association call :key:

--- brine_cloud_functions/functions/src/addDeviceToUser.cjs
+++ brine_cloud_functions/functions/src/addDeviceToUser.cjs
@@ -52,9 +52,10 @@ async function addDeviceToUser(req, res) {
     // middleware.
     const userUid = req.user.uid;
 
-    // Extract the device ID and device name from the request
+    // Extract the device ID, device name, and public key from the request
     const deviceId = req.body.deviceId;
     const deviceName = req.body.deviceName;
+    const publicKey = req.body.publicKey;
 
     console.log('Adding device to user', userUid, ':', deviceId, ';', deviceName);
 
@@ -67,6 +68,11 @@ async function addDeviceToUser(req, res) {
     } else if (!deviceName) {
         console.error('Device name is required');
         res.status(400).send('Device name is required');
+
+        return;
+    } else if (!publicKey) {
+        console.error('Public key is required');
+        res.status(400).send('Public key is required');
 
         return;
     }
@@ -113,7 +119,7 @@ async function addDeviceToUser(req, res) {
     } catch (error) {
         // If an error occurs, log it and return an internal server error.
         console.error('Error adding device to user:', error);
-        
+
         res.status(500).send('Internal Server Error');
 
         return;
@@ -135,6 +141,7 @@ async function addDeviceToUser(req, res) {
                 battery_level: -1,  // -1 indicates unknown battery level
                 salt_level: -1,    // -1 indicates unknown salt level
                 appliance_height: -1,  // -1 indicates unknown appliance height
+                public_key: publicKey,
                 last_updated: new Date().toISOString(),
             });
 
